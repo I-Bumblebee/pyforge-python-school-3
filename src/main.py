ee6b0d6a0f23b61ca9f25693762d97b66c60098e
luka trapaidze <trapaidze.luka@kiu.edu.ge>
2024-07-18 19:48:18 +0400

    feat: implement substructure search algorithm and tests

--- src/main.py
+++ src/main.py
@@ -1,2 +1,15 @@
+import pytest
+from rdkit import Chem
+
+
 def substructure_search(mols, mol):
-    pass
+    substructure = Chem.MolFromSmiles(mol)
+    return list(filter(lambda smiles: Chem.MolFromSmiles(smiles).HasSubstructMatch(substructure), mols))
+
+
+@pytest.mark.parametrize("mols, substructure, expected", [
+    (["CCO", "c1ccccc1", "CC(=O)O", "CC(=O)Oc1ccccc1C(=O)O"],
+     "c1ccccc1", ["c1ccccc1", "CC(=O)Oc1ccccc1C(=O)O"]),
+])
+def test_substructure_search(mols, substructure, expected):
+    assert substructure_search(mols, substructure) == expected
