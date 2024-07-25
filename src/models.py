655c9a495c7dc1652855b4771a34ac2079fd04fe
luka trapaidze <trapaidze.luka@kiu.edu.ge>
2024-07-25 20:15:14 +0400

    feat: implement Molecule model with sqlalchemy

---
+++ src/models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Molecule(Base):
+    __tablename__ = "molecules"
+    id = Column(Integer, primary_key=True, index=True)
+    identifier = Column(String, unique=True, index=True)
+    smiles = Column(Text, nullable=False)
