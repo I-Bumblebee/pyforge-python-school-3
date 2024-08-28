06263b61352a66c8830265d30d879406e6490a6e
luka trapaidze <trapaidze.luka@kiu.edu.ge>
2024-08-28 17:12:09 +0400

    feat: remove unused settings and load .env.testing when APP_ENV=testing

--- src/config.py
+++ src/config.py
@@ -1,3 +1,5 @@
+from os import getenv
+
 from pydantic_settings import (
     BaseSettings,
     SettingsConfigDict,
@@ -5,16 +7,15 @@ from pydantic_settings import (
 
 
 class Settings(BaseSettings):
-    postgres_user: str
-    postgres_password: str
-    postgres_db: str
-    postgres_host: str
-    postgres_port: int
     database_url: str
 
     model_config = SettingsConfigDict(
         case_sensitive=False,
-        env_file=".env",
+        env_file=(
+            ".env.testing"
+            if getenv("APP_ENV") == "testing"
+            else ".env"
+        ),
         env_file_encoding="utf-8",
         extra="ignore",
     )
