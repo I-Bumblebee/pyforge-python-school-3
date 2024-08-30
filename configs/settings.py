9ad040aebfaf793410aab9735289ecc1c9c568e2
luka trapaidze <trapaidze.luka@kiu.edu.ge>
2024-08-30 23:20:02 +0400

    refactor: move settings and database config files in dedicated directory

--- configs/settings.py
+++ configs/settings.py
@@ -1,9 +1,6 @@
 from os import getenv
 
-from pydantic_settings import (
-    BaseSettings,
-    SettingsConfigDict,
-)
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
