88c8427d241c037d9881e5d127816d1761c67f57
luka trapaidze <trapaidze.luka@kiu.edu.ge>
2024-09-05 22:05:50 +0400

    feat: implement cache set get methods

---
+++ configs/redis.py
@@ -0,0 +1,14 @@
+import redis
+import json
+from .settings import settings
+
+redis_client = redis.Redis(port=settings.redis_port)
+
+def get_cached_result(key: str):
+    result = redis_client.get(key)
+    if result:
+        return json.loads(result)
+    return None
+
+def set_cache(key: str, value: dict | list, expiration: int = settings.cache_duration):
+    redis_client.setex(key, expiration, json.dumps(value))
