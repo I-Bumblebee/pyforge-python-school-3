e4d9946a6c8015e31bca3ec02c98608622f1a564
luka trapaidze <trapaidze.luka@kiu.edu.ge>
2024-09-05 22:06:45 +0400

    fix: resolve flake issues

--- configs/redis.py
+++ configs/redis.py
@@ -4,11 +4,15 @@ from .settings import settings
 
 redis_client = redis.Redis(port=settings.redis_port)
 
+
 def get_cached_result(key: str):
     result = redis_client.get(key)
     if result:
         return json.loads(result)
     return None
 
-def set_cache(key: str, value: dict | list, expiration: int = settings.cache_duration):
+
+def set_cache(
+    key: str, value: dict | list, expiration: int = settings.cache_duration
+):
     redis_client.setex(key, expiration, json.dumps(value))
