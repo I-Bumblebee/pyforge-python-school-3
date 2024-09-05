9512b4ceffc62a867150904a10e04666e448ba0f
luka trapaidze <trapaidze.luka@kiu.edu.ge>
2024-09-05 22:05:31 +0400

    feat: add and configure redis service

--- configs/settings.py
+++ configs/settings.py
@@ -5,6 +5,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     database_url: str
+    redis_port: int
+    cache_duration: int
 
     model_config = SettingsConfigDict(
         case_sensitive=False,
